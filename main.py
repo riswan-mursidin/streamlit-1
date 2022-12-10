9ad86b513c77a590215352cab053013807f84947
Riswan Mursidin <89274291+riswan-mursidin@users.noreply.github.com>
2022-12-10 11:15:10 +0000

    Menghitung minimum order

--- main.py
+++ main.py
@@ -8,9 +8,22 @@ import pandas as pd
 # Judul APP
 st.title ("Aplikasi perhitungan percetakan")
 
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
+# Input parameter panjang dan lebar
+lebar_bahan = st.number_input("Masukkan lebar bahan")
+panjang = st.number_input("Masukkan panjang cm")
+lebar = st.number_input("Masukkan lebar cm")
+harga_bahan = st.number_input ("Masukkan harga bahan")
+min_order = st.button ("Hitung minimum order")
 
-df
+if lebar > panjang :
+    minimum = lebar_bahan / panjang
+else :
+    minimum = lebar_bahan / lebar
+
+    st.write ("Minimum order =", minimum)
+
+# Konversi cm ke m
+fix_panjang = panjang / 10000
+fix_lebar = lebar / 10000
+
+# Hasil perhitungan
